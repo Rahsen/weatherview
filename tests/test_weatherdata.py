5b810f2f8441a4e77d963b7304d8f0710b85e30f
rahsen <rahsen@users.noreply.github.com>
2016-07-07 18:06:40 +0100

    WeatherData producing stats for requested city

--- tests/test_weatherdata.py
+++ tests/test_weatherdata.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import weatherdata
+from datetime import datetime
 
 class TestWeatherData(unittest.TestCase):
 
@@ -8,16 +9,56 @@ class TestWeatherData(unittest.TestCase):
         forecastio_apikey = os.environ["FORECASTIO_API_KEY"]
         google_apikey = os.environ["GOOGLE_API_KEY"]
         self.wx = weatherdata.WeatherData(forecastio_apikey, google_apikey)
+        self.today = datetime.utcnow().date().isoformat()
 
     def test_instantiation(self):
         self.assertIsNotNone(self.wx)
 
     def test_get_todays_stats_for_london(self):
-        data = self.wx.get_stats('london')
-        self.assertIsNotNone(data)
-        self.assertIn('city', data)
-        self.assertIn('datefrom', data)
-        self.assertIn('dateto', data)
+        results = self.wx.get_stats('london')
+        self.assertIsNotNone(results)
+        self.assertEqual(len(results), 1)
+        result = results[0]
+        self.assertEqual(result['address'], 'London, UK')
+        self.assertEqual(result['lat'], 51.5073509)
+        self.assertEqual(result['lng'], -0.1277583)
+        date_today = self.today
+        stats = result['stats']
+        self.assertEqual(stats['datefrom'], date_today)
+        self.assertEqual(stats['dateto'], date_today)
+        self._valid_temp(stats['temp_min'])
+        self._valid_temp(stats['temp_max'])
+        self._valid_temp(stats['temp_avg'])
+        self._valid_temp(stats['temp_med'])
+        self._valid_hum(stats['hum_min'])
+        self._valid_hum(stats['hum_max'])
+        self._valid_hum(stats['hum_avg'])
+        self._valid_hum(stats['hum_med'])
+
+    def test_get_dates_today(self):
+        dates = self.wx._get_dates()
+        self.assertEqual(len(dates), 1)
+        self.assertEqual(dates[0], self.today)
+
+    def test_get_dates_range(self):
+        dates = self.wx._get_dates("2016-06-29", "2016-07-02")
+        self.assertEqual(len(dates), 4)
+        self.assertEqual(dates[0], "2016-06-29")
+        self.assertEqual(dates[1], "2016-06-30")
+        self.assertEqual(dates[2], "2016-07-01")
+        self.assertEqual(dates[3], "2016-07-02")
+
+    def test_get_raw_weather_data(self):
+        dataset = self.wx._get_weather_raw('51.0', '0.0')
+        self.assertEqual(len(dataset), 1)
+        date, data = dataset[0]
+        self.assertEqual(date, self.today)
+
+    def _valid_temp(self, temp):
+        self.assertTrue(temp > 0.0 and temp <= 35.0)
+
+    def _valid_hum(self, hum):
+        self.assertTrue(hum > 0.0 and hum <= 1.0)
 
 class TestGeocode(unittest.TestCase):
 
@@ -31,3 +72,4 @@ class TestGeocode(unittest.TestCase):
         name, lat, lng = locations[0]
         self.assertEqual(lat, 51.5073509)
         self.assertEqual(lng, -0.1277583)
+
--- weatherdata/__init__.py
+++ weatherdata/__init__.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import statistics
+from datetime import datetime, timedelta
 
 class Geocode:
 
@@ -9,18 +11,17 @@ class Geocode:
 
     def get(self, city):
         locations = []
-        url = self._makeurl(city)
+        url = self._make_url(city)
         r = requests.get(url)
         if r.status_code == 200:
             data = r.json()
             if data.get('status', '') == 'OK':
                 results = data.get('results', [])
                 locations = self._parse_locations(results)
-        print(locations)
         return locations
 
 
-    def _makeurl(self, address):
+    def _make_url(self, address):
         url = r'{}/json?address={}&key={}'.format(self.baseurl, address, self.apikey)
         return url
 
@@ -38,15 +39,113 @@ class Geocode:
         return locations
 
 
+class WeatherStats:
+
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self._stats = {}
+        self._temps = []
+        self._hums = []
+
+    def process_data(self, dataset):
+        self.clear()
+        for date, data in dataset:
+            if 'datefrom' not in self._stats:
+                self._stats['datefrom'] = date
+            self._stats['dateto'] = date
+
+            if 'lat' not in self._stats and 'latitude' in data:
+                self._stats['lat'] = data['latitude']
+
+            if 'lng' not in self._stats and 'longitude' in data:
+                self._stats['lat'] = data['longitude']
+
+            hourly_data = data.get('hourly', {})
+            data_points = hourly_data.get('data', [])
+            for data_point in data_points:
+                if 'time' in data_point:
+                    dp_date = datetime.fromtimestamp(data_point['time']).date().isoformat()
+                    if dp_date == date:
+                        self._process_data_point(data_point)
+                    else:
+                        print("...skipping", dp_date, date)
+
+    def _process_data_point(self, data):
+        if 'temperature' in data:
+            self._temps.append(float(data['temperature']))
+        if 'humidity' in data:
+            self._hums.append(float(data['humidity']))
+
+    def get(self):
+        if len(self._temps) > 0:
+            self._stats['temp_min'] = min(x for x in self._temps)
+            self._stats['temp_max'] = max(x for x in self._temps)
+            self._stats['temp_avg'] = statistics.mean(self._temps)
+            self._stats['temp_med'] = statistics.median(self._temps)
+        if len(self._hums) > 0:
+            self._stats['hum_min'] = min(x for x in self._hums)
+            self._stats['hum_max'] = max(x for x in self._hums)
+            self._stats['hum_avg'] = statistics.mean(self._hums)
+            self._stats['hum_med'] = statistics.median(self._hums)
+        return self._stats
+
+
 class WeatherData:
 
     def __init__(self, forecastio_apikey, google_apikey):
         self.apikey = forecastio_apikey
         self.geo = Geocode(google_apikey)
+        self.baseurl = r'https://api.forecast.io/forecast'
 
-    def get_stats(city, date=""):
-        data = {}
-        data['city'] = ""
-        data['datefrom'] = ""
-        data['dateto'] = ""
-        return data
+    def get_stats(self, search, datefrom='', dateto=''):
+        dataset = []
+        locations = self.geo.get(search)
+        if len(locations) > 0:
+            address, lat, lng = locations[0]
+            data = {}
+            data['query'] = search
+            data['address'] = address
+            data['lat'] = lat
+            data['lng'] = lng
+            raw_data = self._get_weather_raw(lat, lng, datefrom, dateto)
+            stats = WeatherStats()
+            stats.process_data(raw_data)
+            data['stats'] = stats.get()
+            dataset.append(data)
+        return dataset
+
+    def _get_weather_raw(self, lat, lng, datefrom='', dateto=''):
+        dates = self._get_dates(datefrom, dateto)
+        dataset = []
+        for date in dates:
+            url = self._make_url(lat, lng, date)
+            r = requests.get(url)
+            if r.status_code == 200:
+                data = r.json()
+                dataset.append((date, data))
+            else:
+                print("Bad status code", r.status_code)
+        return dataset
+
+    def _get_dates(self, datefrom='', dateto=''):
+        dates = []
+        if datefrom == '':
+            datefrom = datetime.utcnow()
+            dateto = datefrom
+        else:
+            datefrom = datetime.strptime(datefrom, '%Y-%m-%d')
+            dateto = datetime.strptime(dateto, '%Y-%m-%d')
+        diff = dateto - datefrom
+        for i in range(diff.days + 1):
+            date = datefrom + timedelta(days=i)
+            dates.append(date.date().isoformat())
+        return dates
+
+    def _make_url(self, lat, lng, date=''):
+        url = r'{}/{}/{},{}'.format(self.baseurl, self.apikey, lat, lng)
+        if date != '':
+            url += r',{}T00:00:00-0000'.format(date)
+        url += r'?units=si&exclude=currently,minutely,daily,alerts,flags'
+        return url
