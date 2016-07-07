1c0d2093707b3395e67288797bbf2dfedb7908e3
rahsen <rahsen@users.noreply.github.com>
2016-07-07 11:46:06 +0100

    Add Geocode functionality + tests

--- tests/test_weatherdata.py
+++ tests/test_weatherdata.py
@@ -3,8 +3,31 @@ import unittest
 import weatherdata
 
 class TestWeatherData(unittest.TestCase):
-    def setUp(self):
-        self.apikey = os.environ["FORECASTIO_API_KEY"]
 
-    def test_env(self):
-        self.assertNotEqual(self.apikey, "")
+    def setUp(self):
+        forecastio_apikey = os.environ["FORECASTIO_API_KEY"]
+        google_apikey = os.environ["GOOGLE_API_KEY"]
+        self.wx = weatherdata.WeatherData(forecastio_apikey, google_apikey)
+
+    def test_instantiation(self):
+        self.assertIsNotNone(self.wx)
+
+    def test_get_todays_stats_for_london(self):
+        data = self.wx.get_stats('london')
+        self.assertIsNotNone(data)
+        self.assertIn('city', data)
+        self.assertIn('datefrom', data)
+        self.assertIn('dateto', data)
+
+class TestGeocode(unittest.TestCase):
+
+    def setUp(self):
+        google_apikey = os.environ["GOOGLE_API_KEY"]
+        self.geo = weatherdata.Geocode(google_apikey)
+
+    def test_london(self):
+        locations = self.geo.get('london')
+        self.assertTrue(len(locations) > 0)
+        name, lat, lng = locations[0]
+        self.assertEqual(lat, 51.5073509)
+        self.assertEqual(lng, -0.1277583)
--- weatherdata/__init__.py
+++ weatherdata/__init__.py
@@ -1,7 +1,52 @@
 import os
 import requests
 
+class Geocode:
+
+    def __init__(self, google_apikey):
+        self.apikey = google_apikey
+        self.baseurl = r'https://maps.googleapis.com/maps/api/geocode'
+
+    def get(self, city):
+        locations = []
+        url = self._makeurl(city)
+        r = requests.get(url)
+        if r.status_code == 200:
+            data = r.json()
+            if data.get('status', '') == 'OK':
+                results = data.get('results', [])
+                locations = self._parse_locations(results)
+        print(locations)
+        return locations
+
+
+    def _makeurl(self, address):
+        url = r'{}/json?address={}&key={}'.format(self.baseurl, address, self.apikey)
+        return url
+
+    def _parse_locations(self, dataset):
+        locations = []
+        for data in dataset:
+            address = data.get('formatted_address', None)
+            geodata = data.get('geometry', {})
+            locdata = geodata.get('location', {})
+            lat = locdata.get('lat', '')
+            lng = locdata.get('lng', '')
+
+            if address != '' and lat != '' and lng != '':
+                locations.append((address, lat, lng))
+        return locations
+
+
 class WeatherData:
 
-    def __init__(self, apikey):
-        self.apikey = apikey
+    def __init__(self, forecastio_apikey, google_apikey):
+        self.apikey = forecastio_apikey
+        self.geo = Geocode(google_apikey)
+
+    def get_stats(city, date=""):
+        data = {}
+        data['city'] = ""
+        data['datefrom'] = ""
+        data['dateto'] = ""
+        return data
